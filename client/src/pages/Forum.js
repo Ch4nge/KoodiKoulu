b6b7d447ae613a940b58aef7c421495e61a51a4c
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-09-05 17:43:26 +0300

    Modify Forum Layout ready

--- client/src/pages/Forum.js
+++ client/src/pages/Forum.js
@@ -1,11 +1,48 @@
 import React from 'react'
-import List  from 'semantic-ui-react'
+import { Container, Icon, Grid, Header, List }  from 'semantic-ui-react'
 
 const Forum = () => {
   return(
-    <List>
-
-    </List>
+    <Grid padded='vertically' verticalAlign='middle' container stackable>
+      <Grid.Column width={16} >
+        <List divided relaxed >
+          <Container>
+            <Header as='h3'> Yleinen keskustelu </Header>
+          </Container>
+          <List.Item>
+            <Icon name='envelope open outline' />
+            <List.Content>
+              <List.Header as='a'> Yleinen keskustelu </List.Header>
+              <List.Description> Uusin postaus: Testipost </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item>
+            <Icon name='envelope open outline' />
+            <List.Content>
+              <List.Header as='a'> Ohjelmointi </List.Header>
+              <List.Description> Uusin postaus: Testipost </List.Description>
+            </List.Content>
+          </List.Item>
+          <Container style={{ paddingTop: '1em' }}>
+            <Header as='h3'> Kurssit </Header>
+          </Container>
+          <List.Item>
+            <Icon name='envelope open outline' />
+            <List.Content>
+              <List.Header as='a'> Javan perusteet </List.Header>
+              <List.Description> Uusin postaus: Testipost </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item>
+            <Icon name='envelope open outline' />
+            <List.Content>
+              <List.Header as='a'> Go perusteet </List.Header>
+              <List.Description> Uusin postaus: Testipost </List.Description>
+            </List.Content>
+          </List.Item>
+        </List>
+      </Grid.Column>
+    </Grid>
   )
 }
 
