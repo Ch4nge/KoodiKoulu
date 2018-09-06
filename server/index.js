5f99401406e22f15509e99ba241b879a37f3c507
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-09-06 17:15:59 +0300

    Add initial backend

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+const extractToken = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    req.token = authorization.substring(7)
+  }
+  next()
+}
+
+app.use(extractToken)
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/login', loginRouter)
+app.use(express.static('build'))
+
+mongoose.connect(config.mongoUrl)
+  .then( () => {
+    console.log('connect to database') 
+  })
+  .catch( (e) => {
+    console.log(e) 
+  })
+
+const PORT = config.port
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
